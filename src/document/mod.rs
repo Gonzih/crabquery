5440033a6b08c6b05c940dcb886a895a1cc53b1f
Max Gonzih <gonzih@gmail.com>
2020-02-20 16:52:29 -0500

    Matching works

--- src/document/mod.rs
+++ src/document/mod.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 use html5ever::driver::ParseOpts;
 use html5ever::parse_document;
 use html5ever::tendril::TendrilSink;
@@ -10,6 +9,7 @@ use std::default::Default;
 use std::rc::Rc;
 
 pub struct Document {
+    //{{{
     doc: RcDom,
 }
 
@@ -30,7 +30,7 @@ impl From<&str> for Document {
             .read_from(&mut input.as_bytes())
             .expect("could not parse html input");
 
-        Document { doc }
+        Self { doc }
     }
 }
 
@@ -41,7 +41,7 @@ impl From<String> for Document {
             .read_from(&mut input.as_bytes())
             .expect("could not parse html input");
 
-        Document { doc }
+        Self { doc }
     }
 }
 
@@ -56,25 +56,123 @@ impl From<String> for Document {
 //     }
 // }
 
+impl Document {
+    pub fn select(&self, selector: &str) -> Vec<Element> {
+        let sel = Selector::from(selector);
+        sel.find(self.doc.document.children.borrow())
+    }
+} //}}}
+
+#[derive(Debug, PartialEq)]
+struct Matcher {
+    //{{{
+    tag: Vec<String>,
+    class: Vec<String>,
+    id: Vec<String>,
+}
+
+impl From<String> for Matcher {
+    fn from(input: String) -> Self {
+        Self::from(&input[..])
+    }
+}
+
+impl From<&str> for Matcher {
+    fn from(input: &str) -> Self {
+        let mut segments = vec![];
+        let mut buf = "".to_string();
+
+        for c in input.chars() {
+            if c == '#' || c == '.' || c == '[' {
+                segments.push(buf);
+                buf = "".to_string();
+            }
+            buf.push(c);
+        }
+        segments.push(buf);
+
+        let mut res = Self {
+            tag: vec![],
+            class: vec![],
+            id: vec![],
+        };
+
+        for segment in segments {
+            if segment.len() > 0 {
+                match segment.chars().next() {
+                    Some('#') => res.id.push(segment[1..].to_string()),
+                    Some('.') => res.class.push(segment[1..].to_string()),
+                    None => {}
+                    _ => res.tag.push(segment),
+                }
+            }
+        }
+
+        res
+    }
+}
+//}}}
+
+#[derive(Debug, PartialEq)]
 struct Selector {
+    //{{{
     next: Option<Rc<Selector>>,
-    css: String,
+    matcher: Matcher,
 }
 
 impl From<&str> for Selector {
     fn from(input: &str) -> Self {
-        let mut sepcss = input.split_whitespace();
+        let mut selectors: Vec<_> = input
+            .split_whitespace()
+            .map(|s| Self {
+                matcher: Matcher::from(s),
+                next: None,
+            })
+            .collect();
 
-        Selector {
-            css: sepcss.next().unwrap().to_string(),
-            next: None,
-        }
+        selectors.pop().unwrap()
     }
+}
+
+fn get_attr(attrs: &Ref<'_, Vec<Attribute>>, name: &str) -> Option<String> {
+    attrs
+        .iter()
+        .filter(|attr| &attr.name.local == name)
+        .take(1)
+        .map(|attr| attr.value.to_string().to_owned())
+        .collect::<Vec<_>>()
+        .pop()
 }
 
 impl Selector {
     fn matches(&self, name: &QualName, attrs: Ref<'_, Vec<Attribute>>) -> bool {
-        name.local.to_string() == self.css
+        let mut id_match = true;
+        if let Some(el_id) = get_attr(&attrs, "id") {
+            let el_ids: Vec<_> = el_id.split_whitespace().collect();
+            id_match = self
+                .matcher
+                .id
+                .iter()
+                .all(|id| el_ids.iter().any(|eid| eid == id))
+        }
+
+        let mut class_match = true;
+        if let Some(el_class) = get_attr(&attrs, "class") {
+            let el_classes: Vec<_> = el_class.split_whitespace().collect();
+
+            class_match = self
+                .matcher
+                .class
+                .iter()
+                .all(|class| el_classes.iter().any(|eclass| eclass == class))
+        }
+
+        self.matcher
+            .tag
+            .iter()
+            .any(|tag| &name.local.to_string() == tag)
+            && id_match
+            && class_match
     }
 
     fn find(&self, elements: Ref<'_, Vec<Handle>>) -> Vec<Element> {
@@ -97,30 +195,17 @@ impl Selector {
 
         res
     }
-}
-
-impl Document {
-    pub fn select(&self, selector: &str) -> Vec<Element> {
-        let sel = Selector::from(selector);
-        sel.find(self.doc.document.children.borrow())
-    }
-}
+} //}}}
 
 pub struct Element {
+    //{{{
     handle: Handle,
 }
 
 impl Element {
     pub fn attr(&self, name: &str) -> Option<String> {
         match self.handle.data {
-            NodeData::Element { ref attrs, .. } => attrs
-                .borrow()
-                .iter()
-                .filter(|attr| attr.name.local.to_string() == name)
-                .take(1)
-                .map(|attr| attr.value.to_string().clone())
-                .collect::<Vec<_>>()
-                .pop(),
+            NodeData::Element { ref attrs, .. } => get_attr(&attrs.borrow(), name),
             _ => None,
         }
     }
@@ -131,19 +216,76 @@ impl Element {
             _ => None,
         }
     }
-}
+} //}}}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_from_str() {
+    fn test_document_from_str() {
         Document::from("<a>hi there</a>");
         assert!(true);
     }
 
-    // Element tests
+    // Matcher tests
+    #[test]
+    fn test_matcher_tag() {
+        let m = Matcher::from("a");
+        assert_eq!(
+            m,
+            Matcher {
+                tag: vec!["a".to_string()],
+                class: vec![],
+                id: vec![],
+            }
+        );
+    }
+
+    // Matcher tests
+    #[test]
+    fn test_matcher_complex() {
+        let m = Matcher::from("a.link.another_class#idofel.klass");
+        assert_eq!(
+            m,
+            Matcher {
+                tag: vec!["a".to_string()],
+                class: vec![
+                    "link".to_string(),
+                    "another_class".to_string(),
+                    "klass".to_string()
+                ],
+                id: vec!["idofel".to_string()],
+            }
+        );
+    }
+
+    // // Selector tests{{{
+    // #[test]
+    // fn test_selector_parse_simple() {
+    //     let sel = Selector::from("a");
+    //     assert_eq!(
+    //         sel,
+    //         Selector {
+    //             css: "a".to_string(),
+    //             next: None
+    //         }
+    //     );
+    // }
+
+    // #[test]
+    // fn test_selector_parse_simple_with_class() {
+    //     let sel = Selector::from("a.link");
+    //     assert_eq!(
+    //         sel,
+    //         Selector {
+    //             css: "a.link".to_string(),
+    //             next: None
+    //         }
+    //     );
+    // } //}}}
+
+    // Element tests{{{
     #[test]
     fn test_el_tag() {
         let doc = Document::from("<a class='link'>hi there</a>");
@@ -167,4 +309,29 @@ mod tests {
         let el = sel.first().unwrap();
         assert_eq!(el.attr("id"), Some("linkilink".to_string()));
     }
+
+    #[test]
+    fn test_el_attr_double_id() {
+        let doc = Document::from("<a class='link' id='linkone linkmain'>hi there</a>");
+        let sel = doc.select("a#linkone#linkmain");
+        let el = sel.first().unwrap();
+        assert_eq!(el.attr("class"), Some("link".to_string()));
+    }
+
+    #[test]
+    fn test_el_attr_double_class() {
+        let doc = Document::from("<a class='link button' id='linkmain'>hi there</a>");
+        let sel = doc.select("a.link.button");
+        let el = sel.first().unwrap();
+        assert_eq!(el.attr("id"), Some("linkmain".to_string()));
+    }
+
+    #[test]
+    fn test_el_attr_double_class_reverse_order() {
+        let doc = Document::from("<a class='link button' id='linkmain'>hi there</a>");
+        let sel = doc.select("a.button.link");
+        let el = sel.first().unwrap();
+        assert_eq!(el.attr("id"), Some("linkmain".to_string()));
+    }
+    //}}}
 }
