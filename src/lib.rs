1886c6f2f7a7448fac0e14031e07b3f155aa8c80
Max Gonzih <gonzih@gmail.com>
2020-02-21 11:36:17 -0500

    Release 0.1.2 with updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! Library for quick and easy DOM search based on CSS queries for your scraping needs.
+//!
+//! Supported selectors are:
+//! * tag based `span` or `a`
+//! * class based `.button`
+//! * id based `#mainbutton`
+//! * direct child `>`
+//! * attribute selectors `[href]`, `[href="specific-value"]`, `[href*="contains-str"]`,
+//! `[href^="begins-with"]`,, `[href$="ends-with"]`
+//! * all combinations of above like `div.container > form#feedback input.button`
 #![crate_name = "crabquery"]
 
 mod document;
